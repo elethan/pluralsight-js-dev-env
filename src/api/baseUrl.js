5f6c25cffc277ddc819898379efb4d4c909fc636
Papageorgiou <GBPapTha@Elekta.com>
2016-11-21 12:09:55 +0000

    Finally understand faker

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,11 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://mysterious-dawn-16770.herokuapp.com/';
+  const inDevelopment = window.location.hostname === 'localhost';
+  return inDevelopment ? 'http://localhost:3001/' : '/';
+
+//  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
 }
 
+/*
 function getQueryStringParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -11,3 +15,4 @@ function getQueryStringParameterByName(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+*/
